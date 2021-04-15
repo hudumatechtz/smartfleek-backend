f7fcd5cf75e35c99729088f75210b09fad8801da
Corama Yunus <coramayunus@gmail.com>
2021-04-15 11:32:03 +0300

    alter: order model and ignored image assets

--- models/order.js
+++ models/order.js
@@ -1,33 +1,38 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const orderModel = new Schema({
-  customer: {
-    customerId: {
-      type: Schema.Types.ObjectId,
-      ref: "Customer",
-      required: true,
+const orderModel = new Schema(
+  {
+    customer: {
+      customerId: {
+        type: Schema.Types.ObjectId,
+        ref: "Customer",
+        required: true,
+      },
+      customerEmail: { type: String, required: true },
+      customerLocation: { type: String, required: false },
+      customerNumber: { type: Number, required: true },
     },
-    customerEmail: { type: String, required: true },
+    products: [
+      {
+        product: { type: Object, required: true },
+        quantity: { type: Number, required: true },
+        amount: { type: Number, required: true },
+      },
+    ],
+    // shop: {
+    //   email: {
+    //     type: String,
+    //     required: true,
+    //   },
+    //   shopId: {
+    //     type: Schema.Types.ObjectId,
+    //     required: true,
+    //     ref: "Shop",
+    //   },
+    // },
   },
-  products: [
-    {
-      product: { type: Object, required: true },
-      quantity: { type: Number, required: true },
-      amount: { type: Number, required: true },
-    },
-  ],
-  // shop: {
-  //   email: {
-  //     type: String,
-  //     required: true,
-  //   },
-  //   shopId: {
-  //     type: Schema.Types.ObjectId,
-  //     required: true,
-  //     ref: "Shop",
-  //   },
-  // },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", orderModel);
